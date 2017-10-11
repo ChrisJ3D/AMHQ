e6f31c3c7ef2b22b90fd47d0e9822779a3ad60d2
Chris Judkins <mail@chrisjudkins.com>
2017-10-11 18:45:44 -0400

    I forgot

---
+++ Assets/Plugins/AMHQNodes/Nodes/Character/AttributesNode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
+
+namespace NodeEditorFramework.Standard
+{
+	[Node (false, "Character/Attributes")]
+	public class AttributesNode : Node 
+	{
+		public const string ID = "AttributesNode";
+		public override string GetID { get { return ID; } }
+
+		public override string Title { get { return "Attributes"; } }
+		public override Vector2 DefaultSize { get { return new Vector2 (120, 230); } }
+
+		[ValueConnectionKnob("Stress", Direction.Out, "Number")]
+		public ValueConnectionKnob stressKnob;
+
+		[ValueConnectionKnob("Charisma", Direction.Out, "Number")]
+		public ValueConnectionKnob charismaKnob;
+
+		[ValueConnectionKnob("Innovation", Direction.Out, "Number")]
+		public ValueConnectionKnob innovationKnob;
+
+		[ValueConnectionKnob("Organisation", Direction.Out, "Number")]
+		public ValueConnectionKnob organisationKnob;
+
+		[ValueConnectionKnob("Knowledge", Direction.Out, "Number")]
+		public ValueConnectionKnob knowledgeKnob;
+
+		[ValueConnectionKnob("Eloquence", Direction.Out, "Number")]
+		public ValueConnectionKnob eloquenceKnob;
+
+		[ValueConnectionKnob("Lowest Attribute", Direction.Out, "Number")]
+		public ValueConnectionKnob lowestAttributeKnob;
+
+		[ValueConnectionKnob("Highest Attribute", Direction.Out, "Number")]
+		public ValueConnectionKnob highestAttributeKnob;
+
+		public Number stress = new Number();
+		public Number charisma = new Number();
+		public Number innovation = new Number();
+		public Number organisation = new Number();
+		public Number knowledge = new Number();
+		public Number eloquence = new Number();
+		public Number highestAttribute = new Number();
+		public Number lowestAttribute = new Number();
+
+		public List<Number> attributes = new List<Number>();
+
+		protected string label = "";
+		
+		public override void NodeGUI () 
+		{
+			base.NodeGUI();
+
+			if (GUI.changed)
+				NodeEditor.curNodeCanvas.OnNodeChange(this);
+		}
+		
+		public override bool Calculate () 
+		{
+			stress = 0f;
+			stressKnob.SetValue<Number>(stress);
+			charismaKnob.SetValue<Number>(charisma);
+			innovationKnob.SetValue<Number>(innovation);
+			organisationKnob.SetValue<Number>(organisation);
+			knowledgeKnob.SetValue<Number>(knowledge);
+			eloquenceKnob.SetValue<Number>(eloquence);
+
+			return true;
+		}
+
+		protected Number GetLowestAttribute() {
+
+			attributes.Clear();
+			attributes.Add(stress);
+			attributes.Add(charisma);
+			attributes.Add(innovation);
+			attributes.Add(organisation);
+			attributes.Add(knowledge);
+			attributes.Add(eloquence);
+
+			return lowestAttribute;
+		
+		}
+	}
+}
--- Assets/Plugins/AMHQNodes/Nodes/Math/LerpNode.cs
+++ Assets/Plugins/AMHQNodes/Nodes/Math/LerpNode.cs
@@ -1,16 +1,89 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
 
-public class LerpNode : MonoBehaviour {
+namespace NodeEditorFramework.Standard
+{
+	[Node (false, "Math/Lerp")]
+	public class LerpNode : Node 
+	{
+		public const string ID = "LerpNode";
+		public override string GetID { get { return ID; } }
 
-	// Use this for initialization
-	void Start () {
+		public override string Title { get { return "Lerp"; } }
+		public override Vector2 DefaultSize { get { return new Vector2 (150, 100); } }
+		public override string description { get { return "The Lerp node (short for \"linear interpolation\") blends between a min and a max value based on its input. The input goes from 0 to 1."; } }
+
+		[ValueConnectionKnob("A", Direction.In, "Number")]
+		public ValueConnectionKnob aKnob;
+
+		[ValueConnectionKnob("B", Direction.In, "Number")]
+		public ValueConnectionKnob bKnob;
+
+		[ValueConnectionKnob("Blend", Direction.In, "Number")]
+		public ValueConnectionKnob blendKnob;
+
+		[ValueConnectionKnob("Output", Direction.Out, "Number")]
+		public ValueConnectionKnob outputKnob;
+
+		public Number blend = new Number();
+		public Number a = new Number();
+		public Number b = new Number();
+		public Number output = new Number();
+
+		protected string label = "";
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		public override void NodeGUI () 
+		{
+			GUILayout.Space(5f);
+
+			GUILayout.BeginHorizontal ();
+			GUILayout.BeginVertical ();
+
+			aKnob.DisplayLayout ();
+			bKnob.DisplayLayout();
+			blendKnob.DisplayLayout();
+
+			GUILayout.EndVertical ();
+			GUILayout.BeginVertical ();
+			
+			outputKnob.DisplayLayout (new GUIContent(label));
+			
+			GUILayout.EndVertical ();
+			GUILayout.EndHorizontal ();
+
+			if (GUI.changed)
+				NodeEditor.curNodeCanvas.OnNodeChange(this);
+		}
 		
+		public override bool Calculate () 
+		{
+			blend = 0.5f;
+			a = 0f;
+			b = 0f;
+
+			output = 0f;
+
+			if (aKnob.connected()) {
+				a = aKnob.GetValue<Number>();
+			}
+
+			if (bKnob.connected()) {
+				b = bKnob.GetValue<Number>();
+			}
+
+			if (blendKnob.connected()) {
+				blend = blendKnob.GetValue<Number>();
+			}
+
+			output = Mathf.Lerp(a,b,blend);
+
+			outputKnob.SetValue<Number>(output);
+
+			label = output.ToStringShort();
+				
+			return true;
+		}
 	}
 }
