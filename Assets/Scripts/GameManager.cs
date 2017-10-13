e6cdafa024c50ac2fedc1fba83cd621a0d6f3a41
Chris Judkins <mail@chrisjudkins.com>
2017-10-13 16:37:11 -0400

    Added test scene for UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject CharacterManager;
+	public GameObject TimeManager;
+	public GameObject UIManager;
+	public GameObject NodeManager;
+	public GameObject InventoryManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
